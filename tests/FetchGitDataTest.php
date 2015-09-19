9ddc3a8fc78efe2b19573c29a2837ad4c9f53669
Dipo Isola <oladipupo.isola@andela.com>
2015-09-19 17:34:08 +0100

    Edit test files

--- tests/FetchGitDataTest.php
+++ tests/FetchGitDataTest.php
@@ -6,21 +6,26 @@ use League\Evangelist\FetchGitData;
 
 class FetchGitTest extends \PHPUnit_Framework_TestCase
 {
+
   /**
-   * Test that returned result is an array
+   * Test that the returned result by FetchGitData::FetchDate() is of type array
    */
     public function test_git_api_result()
     {
         $this->assertEquals(gettype(array()), gettype(FetchGitData::FetchData('dzpo')));
     }
+
   /**
-   * Test for inexistent Github user and Null input
+   * Test for inexistent Github user
    */
     public function test_for_inexistent_user()
     {
         $this->assertEquals("User cannot be found on Github.", FetchGitData::FetchData('kzjk'));
     }
 
+  /**
+   * Test for Null input
+   */
     public function test_for_null_user()
     {
         $this->assertEquals("Github username cannot be null. Please enter a valid username.", FetchGitData::FetchData(''));
