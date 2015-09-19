7caf617be5e476e4645c4f3f159502c1e199c8d2
Dipo Isola <oladipupo.isola@andela.com>
2015-09-19 17:25:13 +0100

    Edit ProcessGitData.php

--- src/ProcessGitData.php
+++ src/ProcessGitData.php
@@ -12,15 +12,15 @@ class ProcessGitData
     public static function ProcessData($username)
     {
         try {
-            $publicRepos = FetchGitData::FetchData($username);
+            $GitData = FetchGitData::FetchData($username);
 
-            if ($publicRepos['public_repos'] >= 21) {
+            if ($GitData['public_repos'] >= 21) {
                 return 'Hey, Most Senior Evangelist! You are the man!';
-            } elseif ($publicRepos['public_repos'] >= 11) {
+            } elseif ($GitData['public_repos'] >= 11) {
                 return 'Good Job, Associate Evangelist!';
-            } elseif ($publicRepos['public_repos'] >= 5) {
+            } elseif ($GitData['public_repos'] >= 5) {
                 return 'You are coming up, Junior Evangelist. Keep it moving!';
-            } elseif ($publicRepos['public_repos'] < 5) {
+            } elseif ($GitData['public_repos'] < 5) {
                 throw new LowRepoNumberException();
             }
         } catch(LowRepoNumberException $e) {
