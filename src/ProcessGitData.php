be5d3199b3e2f546cd7081c0d5a11aacd2f1732b
Dipo Isola <isolaoluwatosin@gmail.com>
2015-09-27 22:16:44 +0100

    Merge corrections

--- src/ProcessGitData.php
+++ src/ProcessGitData.php
@@ -3,23 +3,26 @@
 namespace League\Evangelist;
 
 use League\Evangelist\FetchGitData;
-use Leaugue\Evangelist\Exception\InexistentUserException;
-use Leaugue\Evangelist\Exception\NullUserException;
+use League\Evangelist\Exception\InexistentUserException;
+use League\Evangelist\Exception\NullUserException;
 use League\Evangelist\Exception\LowRepoNumberException;
+
 
 class ProcessGitData
 {
-    public static function ProcessData($username)
+    /**
+     * Returns corresponding remark for user's number of repositories
+     */
+    public static function processData($username)
     {
         try {
-			$GitData = FetchGitData::FetchData($username);
-            if ($GitData['public_repos'] >= 21) {
+			$noOfRepos = FetchGitData::getNoOfRepos($username);			            if ($noOfRepos >= 21) {
                 return 'Hey, Most Senior Evangelist! You are the man!';
-            } elseif ($GitData['public_repos'] >= 11) {
+            } elseif ($noOfRepos >= 11) {
                 return 'Good Job, Associate Evangelist!';
-            } elseif ($GitData['public_repos'] >= 5) {
+            } elseif ($noOfRepos >= 5) {
                 return 'You are coming up, Junior Evangelist. Keep it moving!';
-            } elseif ($GitData['public_repos'] < 5) {
+            } elseif ($noOfRepos < 5) {
                 throw new LowRepoNumberException();
             }
         } catch(LowRepoNumberException $e) {
--- tests/EvangelistStatusTest.php
+++ tests/EvangelistStatusTest.php
@@ -7,11 +7,10 @@ use League\Evangelist\EvangelistStatus;
 
 class EvangelistTest extends \PHPUnit_Framework_TestCase
 {
-  /**
-   * Test that function returns name
-   */
-
-    public function UsernameData()
+    /**
+     * Test that function returns name
+     */
+    public function usernameData()
     {
         return [
             ['andela-vdugeri', 'You are coming up, Junior Evangelist. Keep it moving!'],
@@ -24,9 +23,9 @@ class EvangelistTest extends \PHPUnit_Framework_TestCase
     /**
      * Test for the equality between the expected values and actual values returned by $evangelist->getStatus()
      *
-     * @dataProvider UsernameData
+     * @dataProvider usernameData
      */
-    public function test_Evangelist_getStatus_result($username, $output)
+    public function test_evangelist_getstatus_result($username, $output)
     {
         $evangelist = new EvangelistStatus($username);
         $this->assertEquals($output, $evangelist->getStatus());
